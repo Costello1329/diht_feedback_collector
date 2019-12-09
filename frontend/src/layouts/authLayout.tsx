d01d44a8d2f5f90a39cb5286d9e45c00ab3b2c8b
Konstantin Leladze <leladze.kg@phystech.edu>
2019-12-09 22:25:30 +0300

    routing

--- frontend/src/layouts/authLayout.tsx
+++ frontend/src/layouts/authLayout.tsx
@@ -1,11 +1,30 @@
 import React, {Component} from "react";
-import { BrowserRouter as Router} from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import {RegistrationForm} from "../components/RegistrationFormComponent";
 import {AuthorizationForm} from "../components/AuthorizationFormComponent";
 
 
 export class AuthLayout extends Component {
   render () {
-    return <RegistrationForm />
+    const layout: JSX.Element =
+      <Router>
+        <div>
+          <Switch>
+            <Route path = "/registration">
+              <RegistrationForm />
+            </Route>
+            <Route path = "/authorization">
+              <AuthorizationForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    
+    return layout;
   }
 }
