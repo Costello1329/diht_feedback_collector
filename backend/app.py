67bddd80d44c308f972f8a1729726b1ebdd0476c
Kirill <semennikov1360@mail.ru>
2019-12-10 02:03:30 +0300

    add normal registration

--- backend/app.py
+++ backend/app.py
@@ -3,42 +3,32 @@ from flask import abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager
-from flask_login import UserMixin
+from flask import jsonify
 import uuid
 
 app = Flask(__name__)
-login = LoginManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-class Users(UserMixin, db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     guid = db.Column(db.Integer, db.ForeignKey('guid.guid'))
     role = db.Column(db.String(20), default="student", unique=False, nullable=True)
     login = db.Column(db.String(30), unique=True, nullable=True)
     password = db.Column(db.String(16), unique=False, nullable=True)
-    first_name = db.Column(db.String(30), unique=False, nullable=True)
-    second_name = db.Column(db.String(30), unique=False, nullable=True)
-    middle_name = db.Column(db.String(30), unique=False, nullable=True)
-    email = db.Column(db.String(30), unique=True, nullable=True)
-    token = db.Column(db.Integer, unique=True, nullable=True)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return self.password == password
 
 
 class Guid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    guid = db.Column(db.Integer)
-    group = db.Column(db.String(20), unique=True, nullable=False)
-    active = db.Column(db.BOOLEAN(), unique=True, nullable=False)
+    guid = db.Column(db.Integer, unique=True)
+    group = db.Column(db.String(20), unique=False, nullable=False)
+    active = db.Column(db.BOOLEAN(), default=False, unique=False, nullable=False)
 
 
 '''
@@ -46,12 +36,17 @@ class Guid(db.Model):
 guid:int
 login:string
 password:string
-first_name:string
-second_name:string
-middle_name:string
+confirmation:string
 email:string 
 }
 '''
+
+
+def set_reg_json(isTokenValid,isTokenActivated, isConfirmationValid, isLoginValid):
+    return jsonify(isTokenValid=isTokenValid,
+                   isTokenActivated=isTokenActivated,
+                   isConfirmationValid=isConfirmationValid,
+                   isLoginValid=isLoginValid)
 
 
 @app.route('/reg', methods=['POST'], )
@@ -60,24 +55,30 @@ def reg():
     if user_data is None:
         return abort(400)
     try:
-        guid = Guid.query.filter_by(guid=user_data['guid']).first()
+        guid = Guid.query.filter_by(guid=user_data['token']).first()
         if guid is None:
-            return abort(400)
-        if not guid.active:
-            return abort(400)
+            return set_reg_json(False, "undefined", "undefined", "undefined"), 400
+        if guid.active:
+            return set_reg_json(True, True, "undefined", "undefined"), 400
     except:
-        return abort(400)
+        return abort(500)
     try:
-        User = Users(login=user_data['login'], email=user_data['email'], token=uuid.uuid4().hex)
-        User.set_password(user_data['password'])
+        # uuid.uuid4().hex
+        if user_data["password"] != user_data["confirmation"]:
+            return set_reg_json(True, False, False, "undefined"), 400
+        if Users.query.filter_by(login=user_data['login']).first() is not None:
+            return set_reg_json(True, False, True, False), 400
+        User = Users(login=user_data['login'], password=user_data['password'], guid=guid.guid)
     except:
-        return abort(400)
+        return abort(500)
     try:
         db.session.add(User)
+        guid.active = True
+        db.session.add(guid)
         db.session.commit()
     except:
         return abort(500)
-    return abort(200)
+    return set_reg_json(True, False, True, True), 200
 
 
 @app.route('/login', methods=['POST'])
