2dbae3b79ac0eafd11c4b5f71c8c439b3f865165
Your Name <Semennikov1360@mail.ru>
2020-01-02 19:35:18 +0300

    add models

--- backend/diht_feedback_collector/admin.py
+++ backend/diht_feedback_collector/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import People, Guid, Student, Token
+
+admin.site.register(People)
+admin.site.register(Guid)
+admin.site.register(Student)
+admin.site.register(Token)
--- backend/diht_feedback_collector/models.py
+++ backend/diht_feedback_collector/models.py
@@ -2,17 +2,31 @@ from django.db import models
 
 
 # Create your models here.
+class Guid(models.Model):
+    guid = models.IntegerField()
+    group = models.CharField(max_length=30)
+    active = models.BooleanField(default=False)
+
+
 class People(models.Model):
     guid = models.ForeignKey('Guid', related_name='users_to_guid', on_delete=models.CASCADE)
     role = models.CharField(max_length=20, default='student')
-    login = models.CharField(max_length=30,null=False)
+    login = models.CharField(max_length=30, null=False)
     password = models.CharField(max_length=16)
 
     def check_password(self, password):
         return self.password == password
 
 
-class Guid(models.Model):
-    guid = models.IntegerField()
+class Student(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=40)
+    second_name = models.CharField(max_length=40)
+    middle_name = models.CharField(max_length=40)
     group = models.CharField(max_length=30)
-    active = models.BooleanField(default=False)
+
+
+class Token(models.Model):
+    token = models.CharField(unique=True, primary_key=True,max_length=20)
+    people = models.ForeignKey('People', related_name='people_to_guid', on_delete=models.DO_NOTHING)
+    date = models.DateField()
